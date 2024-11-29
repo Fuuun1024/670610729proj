f9fcf2ba0a6c833015d8adc780eaff67ae703aed
Fuuun1024 <worapopfong@gmail.com>
2024-11-29 17:02:44 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
+    int x=1;
+    int m = 0;
+    int n= 0;
+    
+    while(x!=0)
+    {
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    if(x%2==0){
+        m+=1;
+    }
+    else{
+        n+=1;
+    }
+    
+    }
+    
+    cout << "#Even numbers = "<< m-1 << "\n";
+    cout << "#Odd numbers = "<< n;
+    
     return 0;
 }
